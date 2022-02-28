702f41ff6da3abdec8b1d35a2f8b0295ee3a6f09
jibundeyare <contact@jibundeyare.com>
2022-02-28 16:49:09 +0100

    Fin de l'exercice 02

--- exercice-02-variables.py
+++ exercice-02-variables.py
@@ -11,8 +11,22 @@
 
 # réponse 2.1
 nbr = 42
-nbrdor = 1.61
-name = ""
+print(nbr)
+
+nbr_dor = 1.61
+print(nbr_dor)
+
+name = "Bernon nicolas"
+print(name)
+
+is_morning = True
+print(is_morning)
+
+is_not_morning = False
+print(is_not_morning)
+
+get_number = None
+print(get_number)
 # code 2.1
 # la fonction `round()` permet d'arrondir un float en un integer
 # 0,1 est arrondi à la valeur inférieur
@@ -32,4 +46,18 @@ print(round(1 / 3, 4))
 # À chaque fois stockez le résultat dans une variable et affichez le résultat.
 
 # réponse 2.2
+number = 2
+number = float(number)
+print(number)
 
+number_float1 = 1.62
+number_float = int(number_float1)
+print(number_float1)
+
+number_float2 = 1.62
+number_float2 = round(number_float2, 0)
+print(number_float2)
+
+number_float3 = 1.62
+number_float3 = round(number_float3, 1)
+print(number_float3)
