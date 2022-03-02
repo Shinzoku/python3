35f141768fe6db8556f675ee42f8a1add50132f8
jibundeyare <contact@jibundeyare.com>
2022-03-02 11:58:23 +0100

    Mise à jour cours variables
    Exploration de conversions de type.

--- variables.py
+++ variables.py
@@ -45,3 +45,102 @@ print(multiline_text)
 
 multiline_text = "\\foo\nbar\nbaz"
 print(multiline_text)
+
+number = "5"
+print(number)
+print(type(number))
+number = int(number)
+print(number)
+print(type(number))
+
+number = "2.71"
+print(number)
+print(type(number))
+number = float(number)
+print(number)
+print(type(number))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0.0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0.000001
+my_var = bool(my_var)
+print(my_var)
+
+my_var = ""
+my_var = bool(my_var)
+print(my_var)
+
+my_var = " "
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "0"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "-123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [123, "abc", False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = []
+my_var = bool(my_var)
+print(my_var)
+
+
+my_var = [[]]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [None]
+my_var = bool(my_var)
+print(my_var)
+
+
+my_var = None
+my_var = bool(my_var)
+print(my_var)
