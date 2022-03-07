e33c2ad7b4a856051c51b12c811d3a3b0c04e9cf
jibundeyare <contact@jibundeyare.com>
2022-03-07 20:04:37 +0100

    Mise à jour des exercices sur les boucles
    exercices fini

--- exercice-07-boucles.py
+++ exercice-07-boucles.py
@@ -23,30 +23,50 @@ number = random.randint(1, 10)
 # affichez `r` s'il est égal à 1
 
 # réponse 7.3
+for i in range(1, 101):
+    r = random.randint(1, 10)
+    if r == 1:
+        print(r)
 
 # exo 7.4
 # en utilisant une boucle for, on tire 50 fois un nombre entier `r` au hasard entre 1 et 10 inclus
 # affichez `r` s'il est plus petit ou égal à 5
 
 # réponse 7.4
+for i in range(1, 51):
+    r = random.randint(1, 10)
+    if r <= 5:
+        print(r)
 
 # exo 7.5
 # en utilisant une boucle for, on tire 20 fois un nombre entier `r` au hasard entre 1 et 10 inclus
 # affichez `r` s'il est plus grand ou égal à 6
 
 # réponse 7.5
+for i in range(1, 21):
+    r = random.randint(1, 10)
+    if r >= 6:
+        print(r)
 
 # exo 7.6
 # en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
 # affichez `r` s'il est égal à 1 ou égal à 10
 
 # réponse 7.6
+for i in range(1, 101):
+    r = random.randint(1, 10)
+    if r == 1 or r == 10:
+        print(r)
 
 # exo 7.7
 # en utilisant une boucle for, on tire 10 fois un nombre entier `r` au hasard entre 1 et 10 inclus
 # affichez `r` s'il est compris entre 3 et 8 inclus
 
 # réponse 7.7
+for i in range(1, 11):
+    r = random.randint(1, 10)
+    if 3 <= r <= 8:
+        print(r)
 
 # exo 7.8
 # en utilisant une boucle for, on tire 50 fois un nombre entier `r` au hasard entre 1 et 10 inclus
@@ -54,6 +74,12 @@ number = random.randint(1, 10)
 # affichez la variable `count`
 
 # réponse 7.8
+count = 0
+for i in range(1, 51):
+    r = random.randint(1, 10)
+    if r == 7:
+        count += 1
+print(count)
 
 # exo 7.9
 # en utilisant une boucle for, on tire 10 fois un nombre entier `r` au hasard entre 1 et 10 inclus
@@ -61,6 +87,12 @@ number = random.randint(1, 10)
 # affichez la variable `count`
 
 # réponse 7.9
+count = 0
+for i in range(1, 11):
+    r = random.randint(1, 10)
+    if r <= 4:
+        count += 1
+print(count)
 
 # exo 7.10
 # en utilisant une boucle for, on tire 33 fois un nombre entier `r` au hasard entre 1 et 10 inclus
@@ -68,6 +100,12 @@ number = random.randint(1, 10)
 # affichez la variable `count`
 
 # réponse 7.10
+count = 0
+for i in range(1, 34):
+    r = random.randint(1, 10)
+    if r >= 7:
+        count += 1
+print(count)
 
 # exo 7.11
 # en utilisant une boucle for, on tire 66 fois un nombre entier `r` au hasard entre 1 et 10 inclus
@@ -75,6 +113,12 @@ number = random.randint(1, 10)
 # affichez la variable `count`
 
 # réponse 7.11
+count = 0
+for i in range(1, 67):
+    r = random.randint(1, 10)
+    if r <= 2 or r == 9:
+        count += 1
+print(count)
 
 # exo 7.12
 # en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
@@ -82,21 +126,36 @@ number = random.randint(1, 10)
 # affichez la variable `count`
 
 # réponse 7.12
+count = 0
+for i in range(1, 101):
+    r = random.randint(1, 10)
+    if 2 <= r <= 9:
+        count += 1
+print(count)
 
 # exo 7.13
 # en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 99 inclus
 
 # réponse 7.13
+for i in range(1, 100):
+    if i % 2 == 0:
+        print(i)
 
 # exo 7.14
 # en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 100 inclus
 
 # réponse 7.14
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
 
 # exo 7.15
 # en utilisant une boucle for, affichez tous les nombres divisibles par 3, de 2 à 99 inclus
 
 # réponse 7.15
+for i in range(2, 100):
+    if i % 3 == 0:
+        print(i)
 
 # code 7.2
 # pour calculer la puissance 2 d'un nombre, on peut le multiplier par lui-même
@@ -109,6 +168,8 @@ print(number ** 2)
 # en utilisant une boucle for, affichez la puissance 2 des nombres de 0 à 99 inclus
 
 # réponse 7.16
+for i in range(0, 100):
+    print(i ** 2)
 
 # code 7.3
 # pour calculer la puissance 3 d'un nombre, on peut l'élever à la puissance 3 avec l'opérateur puissance `**`
@@ -119,10 +180,14 @@ print(number ** 3)
 # en utilisant une boucle for, affichez la puissance 3 des nombres de 1 à 100 inclus
 
 # réponse 7.17
+for i in range(1, 101):
+    print(i ** 3)
 
 # exo 7.18
 # dans une boucle while, on tire un nombre entier `r` au hasard entre 1 et 100 inclus
 # boucler jusqu'à ce que la valeur 100 soit tirée au hasard
 
 # réponse 7.18
-
+while r != 100:
+    r = random.randint(1, 100)
+    print("valeur de r: " + str(r))
