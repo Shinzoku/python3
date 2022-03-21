c40df27ed5a16a1ab2855dc4c6fc619638cb0566
shinzoku <shinzoku62800@gmail.com>
2022-03-21 11:08:29 +0100

    Mise à jour
    Modification de consigne (tableau -> liste)

--- exercice-05-strings.py
+++ exercice-05-strings.py
@@ -14,7 +14,7 @@ def multiplication(a: float, b: float) -> float:
         """
     return a * b
 
-print(help(multiplication))
+help(multiplication)
 
 # exo 5.2
 # Trouvez la position de la chaîne de caractères `minim`
@@ -45,7 +45,7 @@ print("foo bar baz".split(' '))
 
 # exo 5.4
 # Scindez la variable `my_text` en utilisant l'espace comme séparateur et affectez le résultat à une autre variable.
-# Affichez le mot qui se trouve à l'index 5 du tableau ; le résultat devrait être `consectetur`
+# Affichez le mot qui se trouve à l'index 5 de la liste; le résultat devrait être `consectetur`
 my_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -83,5 +83,5 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 
 # réponse 5.5
 
-nbr_line = len(my_text.split(".\n"))
+nbr_line = len(my_text.split("\n"))
 print(nbr_line)
