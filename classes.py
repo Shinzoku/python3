7b6f3285c8b233026ea31ffb84645aa5f57d4864
shinzoku <shinzoku62800@gmail.com>
2022-05-03 14:04:46 +0200

    Ajout du cours sur les classes
    Ce fichier est lié avec camion.py, voiture.py et vehicule.py

---
+++ classes.py
@@ -0,0 +1,80 @@
+from voiture import Voiture # on peut ajouter un alias en cas de classe doublon: "as Car", à coté de Voiture
+from camion import Camion
+
+c1 = Camion("Scania", "?", "diesel", 0, 19.0)
+print(c1)
+c2 = Camion("Mercedes", "?", "diesel", 0, 19.0)
+print(c2)
+
+v1 = Voiture("BMW", "Série 5", "diesel", 0, "berline")
+# v1 affiche "voiture.Voiture object at 0x7fef1ecfc910" si la fonction __str__() n'est pas définie
+print(v1)
+# Il faut pas afficher directement ces variables car elles sont considérées comme si elles étaient privées.
+# C-à-d qu'elles sont toutes préfixées d'un underscore "_"
+# print(v1.marque, v1.modele, v1.carburant, v1.type_carrosserie, v1.get_vitesse())
+print(v1.get_marque())
+print(v1.get_modele())
+print(v1.get_carburant())
+print(v1.get_type_carrosserie())
+
+print(v1.get_vitesse())
+v1.set_vitesse(10)
+print(v1.get_vitesse())
+
+v2 = Voiture("Ford", "Mustang 68", "essence", 0, "muscle car")
+print(v2)
+print(v2.get_vitesse())
+# transmettre une valeur de type autre que int génère l'erreur: "Exception: La vitesse doit être un nombre entier"
+v2.set_vitesse(50)
+print(v2.get_vitesse())
+print(v1.get_vitesse())
+
+my_list = []
+my_list.append(v1)
+my_list.append(v2)
+my_list.append(c1)
+my_list.append(c2)
+
+for item in my_list:
+    print(item._marque)
+    print(item.get_vitesse())
+
+    if type(item) is Voiture:
+        # code spécifique au voitures
+        print(item._type_carrosserie)
+        print(item.get_type_carrosserie())
+
+    if type(item) is Camion:
+        # code spécifique aux camions
+        print(item._ptac)
+        print(item.get_ptac())
+
+for i in range(0, len(my_list)):
+    print(my_list[i]._marque)
+    print(my_list[i].get_vitesse())
+
+    if type(my_list[i]) is Voiture:
+        # code spécifique au voitures
+        print(my_list[i]._type_carrosserie)
+        print(my_list[i].get_type_carrosserie())
+
+    if type(my_list[i]) is Camion:
+        # code spécifique aux camions
+        print(my_list[i]._ptac)
+        print(my_list[i].get_ptac())
+
+# modification d'une variable de classe
+# cette variable sera modifiée pour toutes les variables de type Voiture
+Voiture._acceleration = 40
+
+print(v1.get_vitesse())
+v1.accelerer()
+print(v1.get_vitesse())
+
+print(v2.get_vitesse())
+v2.accelerer()
+print(v2.get_vitesse())
+
+print(c1.get_vitesse())
+c1.accelerer()
+print(c1.get_vitesse())
