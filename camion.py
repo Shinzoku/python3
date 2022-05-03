774214777f59ce378d7cdbdf9d874f655db73d75
shinzoku <shinzoku62800@gmail.com>
2022-05-03 14:07:29 +0200

    Ajout de l'objet camion.py
    Cette objet est lié avec classes.py

---
+++ camion.py
@@ -0,0 +1,25 @@
+from vehicule import Vehicule
+
+class Camion(Vehicule):
+    """
+    Cette classe représente un camion.
+    """
+
+    # redéfinir une méthode (fonction) d'une classe mère dans une classe enfant s'appelle "surcharger une méthode"
+    def __init__(self, marque: str, modele: str, carburant: str, vitesse: int, ptac: float):
+        super().__init__(marque, modele, carburant, vitesse)
+        # il faut utiliser le setter s'il y a une procédure de vérification des données avant l'affectation
+        self.set_ptac(ptac)
+
+    # redéfinir une méthode (fonction) d'une classe mère dans une classe enfant s'appelle "surcharger une méthode"
+    def __str__(self) -> str:
+        return super().__str__() + f" {self._ptac}"
+
+    def get_ptac(self) -> float:
+        return self._ptac
+
+    def set_ptac(self, ptac: float):
+        if type(ptac) != float:
+            raise Exception("Le ptac doit être un float")
+
+        self._ptac = ptac
